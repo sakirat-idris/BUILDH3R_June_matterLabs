560aabc219a435af251e5d7d652a8130ee863253
sakirat-idris <sakiratidris93@gmail.com>
2024-06-13 20:02:52 -0700

    Update paymaster-transaction.ts

--- scripts/paymaster-transaction.ts
+++ scripts/paymaster-transaction.ts
@@ -6,9 +6,9 @@ import TokenArtifact from "../artifacts/TestToken";
 import ZeekMessagesArtifact from "../artifacts/ZeekSecretMessages";
 
 // Address of the ZeekMessages contract
-const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0xffcc75ccdca5d0fab7c5ddd3f26bddaf4f15dc62";
 // Address of the ERC20 token contract
-const TOKEN_CONTRACT_ADDRESS = "0xf8e81D47203A594245E36C48e151709F0C19fBe8"
+const TOKEN_CONTRACT_ADDRESS = "0xc4c9b2dacbc08f1bbeaa85f4523effcee3ecabc1"
 // Message to be sent to the contract
 const NEW_MESSAGE = "This tx cost me no ETH!";
 
